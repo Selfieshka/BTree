6ee511ae59692f5ad4d65dee2c2d82ade4d4aa9d
Kirill Akhmetov <kirillahmetov935@gmail.com>
2024-05-11 20:09:05 +0300

    added function generates random integer array

--- src/main/java/ru/kpfu/itis/kirillakhmetov/work/Main.java
+++ src/main/java/ru/kpfu/itis/kirillakhmetov/work/Main.java
@@ -1,14 +1,29 @@
 package ru.kpfu.itis.kirillakhmetov.work;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        final int ORDER_TREE = 2;
-        Btree btree = new Btree(ORDER_TREE);
-        int[] arr = new int[] {30, 70, 8, 25, 40, 50, 76, 88, 1, 3, 7, 15, 21, 23, 26, 28, 35, 38, 42, 49, 56, 67, 71, 73, 75, 77, 85, 89, 97};
+        Tree tree = new BTree();
+//        int[] arr = {30, 70, 8, 25, 40, 50, 76, 88, 1, 3, 7, 15, 21, 23, 26, 28, 35, 38, 42, 49, 56, 67, 71, 73, 75, 77, 85, 89, 97};
+        int[] arr = generateRandomIntArray();
 
-        for (int el : arr) {
-            btree.add(el);
+    }
+
+    private static int[] generateRandomIntArray() {
+        final int SIZE = 10000;
+        Set<Integer> generatedNumbers = new HashSet<>();
+        Random random = new Random();
+
+        while (generatedNumbers.size() < SIZE) {
+            generatedNumbers.add(random.nextInt());
         }
-        btree.show();
+
+        return generatedNumbers.stream().mapToInt(Integer::intValue).toArray();
     }
 }
